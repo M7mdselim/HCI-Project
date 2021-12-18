3404dc12e51f702ff400b51c848b898a8071e613
Selim <mohammedsaleem330@gmail.com>
2021-12-18 23:48:48 +0200

    First Activity

---
+++ app/src/main/java/com/example/hci/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.hci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+EditText username;
+EditText password;
+Button login;
+TextView crtNewAcc;
+TextView forgotPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = findViewById(R.id.txt_user);
+        password = findViewById(R.id.txt_pass);
+        login = findViewById(R.id.btn_login);
+
+
+
+
+
+
+    }
+
+    public void click(View view) {
+        setLogin();
+    }
+    public void setLogin(){
+        String user = username.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if (user.equals("selim") && pass.equals("selim")){
+            Toast.makeText(this, "Username and Password Matched", Toast.LENGTH_LONG).show();
+
+
+        }
+        else  {
+            Toast.makeText(this, "Username and Password dosen't Matched", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    public void click_signup(View view) {
+        Intent intent = new Intent(MainActivity.this , MainActivity2.class);
+        startActivity(intent);
+
+    }
+
+    public void click_forgotPasswrod(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/hci/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.hci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button crtnewacc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void click(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+    }
+}
